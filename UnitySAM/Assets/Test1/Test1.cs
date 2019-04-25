4b55578c39d02b328c45830952637850079ba775
Kurt Dekker <kdekker@endymion>
2019-04-25 11:32:23 -0700

    UnitySAM: Test1 module

---
+++ UnitySAM/Assets/Test1/Test1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+	void Start ()
+    {
+        string s = "HELLO";
+
+        var x = UnitySAM.TextToPhonemes(s);
+
+        Debug.Log(x.ToString());
+	}
+}
