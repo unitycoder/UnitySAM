08e3f5d2222c452c3627222ba3165a7e3003cefc
Kurt Dekker <kdekker@endymion>
2019-04-25 15:06:39 -0700

    UnitySAM: simple Buffer object; it's a list but lets YOU keep track of where you are writing, which appears to be the API use case in this code. I am leaving position calculations to the actual code, since it contains intel such as dividing by 50 per phoneme, etc.

---
+++ UnitySAM/Assets/SAMCSourceCode/src/Buffer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buffer
+{
+	List<int> Data;
+
+	public Buffer()
+	{
+		Data = new List<int>();
+	}
+
+	public int[] Get()
+	{
+		return Data.ToArray();
+	}
+
+	public void Set( int position, int data)
+	{
+		while( position >= Data.Count)
+		{
+			Data.Add( 0);
+		}
+		Data[position] = data;
+	}
+}
